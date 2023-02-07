143189c415e62dae3e4fd5a4bbea31ea2f3cc91c
4uruH <97702288+4uruH@users.noreply.github.com>
2023-02-07 13:23:09 +0300

    DZ8 Done!

--- blog/app.py
+++ blog/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from blog.commands import create_admin, create_tags
 from blog.models import User
 from blog.security import flask_bcrypt
-from blog.extensions import db, login_manager, migrate, csrf
+from blog.extensions import db, login_manager, migrate, csrf, admin
 
 
 def create_app() -> Flask:
@@ -23,12 +23,15 @@ def register_blueprints(app: Flask):
     from blog.homepage.views import homepage
     from blog.auth.views import auth_app
     from blog.author.views import authors_app
+    from blog import admin
 
     app.register_blueprint(users_app, url_prefix="/users")
     app.register_blueprint(articles_app, url_prefix="/articles")
     app.register_blueprint(homepage, url_prefix="/")
     app.register_blueprint(auth_app, url_prefix="/auth")
     app.register_blueprint(authors_app, url_prefix="/authors")
+
+    admin.register_views()
 
 
 def register_commands(app: Flask):
@@ -40,6 +43,7 @@ def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db, compare_type=True)
     csrf.init_app(app)
+    admin.init_app(app)
 
     login_manager.login_view = 'auth_app.login'
     login_manager.init_app(app)
